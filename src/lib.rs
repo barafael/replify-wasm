2e741aab40c385b41ad94dd34602b50aca42862f
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-12-15 23:32:46 +0100

    More useful helptext

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,10 @@ use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
 pub fn process_str(input: String) -> String {
-    input
+    input.to_ascii_uppercase()
 }
 
 #[wasm_bindgen]
 pub fn help_text() -> String {
-    "Here's some help text to get started".to_string()
+    "Here's some help text to get started. This example program will just shout back anything you give it".to_string()
 }
